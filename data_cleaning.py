35dd816c2c62a0d73d30f0b8cdaca10f1a59738a
VerrillAng <verrill.ang@gmail.com>
2024-04-07 18:04:14 -0700

    add senitmental_analysis.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -10,8 +10,7 @@ data = pd.read_csv('/Users/kathryntanardy/Desktop/movie-ratings/data_scraped/ant
 
 data['comment'] = data['comment'].apply(lambda x: x.lower())
 print(data)
-# Remove columns that contained removed comments (
-# [removed] or [deleted])
+# Remove columns that contained removed comments ([removed] or [deleted])
 data = data[data['comment'] != '[removed]']
 data = data[data['comment'] != '[deleted]']
 
@@ -21,36 +20,10 @@ data = data[~data['comment'].str.contains('.jpg')]
 data = data[~data['comment'].str.contains('.jpeg')]
 data = data[~data['comment'].str.contains('.png')]
 data = data[~data['comment'].str.contains('.gif')]
-# print(data)
 
+# Remove punctuations
+data['comment_body'] = data['comment_body'].apply(lambda x: x.translate(str.maketrans('','', string.punctuation)))
 
-data['tokens'] = data['comment'].apply(lambda x: word_tokenize(x, "english"))
-# print(data)
-# print(data['tokens'])
-
-# data['pos_tags'] = data['tokens'].apply(pos_tag)
-
-stemmer = PorterStemmer()
-data['stemmed'] = data['tokens'].apply(lambda x: [stemmer.stem(token) for token in x])
-# print(data['stemmed']) 
-
-lemmatizer = WordNetLemmatizer()
-data['lemmatized'] = data['tokens'].apply(lambda x: [lemmatizer.lemmatize(token) for token in x])
-
-
-print(data)
-# #Remove columns that contain language aside from english
-# def english_sentence(text):
-#     try:
-#         return detect(text) == 'en'
-#     except LangDetectException:
-#         return False
-
-# data['english'] = data['comment_body'].apply(english_sentence)
-# data = data[data['english']]
-# data = data.drop(columns=['english'])
-
-# #Remove curse word from the data 
-# data['comment_body'] = data['comment_body'].apply(lambda x: profanity.censor(x, ''))
+# Export to csv
 data.to_csv('example.csv', index=False)
 
